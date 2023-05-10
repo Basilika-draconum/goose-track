046f08ce0463fd5fc6b219c98428d71d6857ebf8
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2023-05-10 19:58:13 +0300

    logout in calendar page

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -5,13 +5,20 @@ import css from './calendar-page.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks } from 'redux/tasks/tasksOperations';
 import { getAccessToken } from 'redux/auth/authSelectors';
+import { logOutThunk } from 'redux/auth/authOperations';
 
 const CalendarPage = () => {
   const dispatch = useDispatch();
   const token = useSelector(getAccessToken);
 
   useEffect(() => {
-     token &&  dispatch(fetchTasks());
+     token &&  dispatch(fetchTasks())
+     .catch((error) => {
+        if (error.status === "401"){
+          dispatch(logOutThunk());
+        }
+      }
+    );
   }, [dispatch, token]);
 
   return (
